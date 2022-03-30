265e7e033ff15d8bb89f241fbeae6f381c7d75b6
aston7331 <aston7332@gmail.com>
2022-03-30 17:21:30 +0530

    Adding ticket form

--- myScript.js
+++ myScript.js
@@ -105,33 +105,33 @@ function scrollFunction() {
     // var classVariable =  document.getElementsByClassName("navcontant");
 
     document.getElementsByClassName("navcontant")[0].classList.add('navListNew');
-    
+
 
 
 
   } else {
 
     document.getElementsByClassName("navcontant")[0].classList.remove('navListNew');
-    
-    
+
+
 
 
   }
 
-//   var classVariable = document.getElementsByClassName("navLists");
-//   for (i = 1; i < classVariable.length; i++) {
+  //   var classVariable = document.getElementsByClassName("navLists");
+  //   for (i = 1; i < classVariable.length; i++) {
 
-//     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-//       document.getElementsByClassName("classVariable")[0].classList.add('listColor');
+  //     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  //       document.getElementsByClassName("classVariable")[0].classList.add('listColor');
 
-// console.log("12345")
+  // console.log("12345")
 
-//     } else {
+  //     } else {
 
-//       document.getElementsByClassName("classVariable")[0].classList.remove('listColor');
-//       console.log("0000")
-//     }
-//   }
+  //       document.getElementsByClassName("classVariable")[0].classList.remove('listColor');
+  //       console.log("0000")
+  //     }
+  //   }
 }
 function toggleMobileMenu(menu) {
   menu.classList.toggle('open');
@@ -140,19 +140,89 @@ function toggleMobileMenu(menu) {
 
 
 
-var screenWith= screen.width;
+var screenWith = screen.width;
 
-function showFooterNav(elementid){
-  if(screenWith < 860){
-    var element= document.getElementById(elementid);
+function showFooterNav(elementid) {
+  if (screenWith < 860) {
+    var element = document.getElementById(elementid);
 
-    if (element.style.display == "block"){
-    
-        // document.getElementById('showContent');
-        element.style.display="none";
-    
+    if (element.style.display == "block") {
+
+      // document.getElementById('showContent');
+      element.style.display = "none";
+
     } else {
-        element.style.display="block";
+      element.style.display = "block";
     }
   }
+}
+// function swap1() {
+//   var data1 = document.getElementById("from").value;
+//   var data2 = document.getElementById("to").value;
+//   document.getElementById("from").value = data2;
+//   document.getElementById("to").value = data1;
+//   document.getElementById("swap").onclcik = function () {swap2()};
+// }
+// function swap2() {
+//   var data1 = document.getElementById("to").value;
+//   var data2 = document.getElementById("from").value;
+//   document.getElementById("to").value = data2;
+//   document.getElementById("to").value = data1;
+//   document.getElementById("swap").onclcik = function () {swap2()};
+// }
+function swap1() {
+
+  var tmp = document.getElementById('from').value;
+  document.getElementById('from').value = document.getElementById('to').value;
+  document.getElementById('to').value = tmp;
+};
+// }
+
+
+function dateValidation() {
+
+  // var obj = document.getElementById("date");
+  // var day = obj.value.split("/")[0];
+
+  // var month = obj.value.split("/")[1];
+  // var year = obj.value.split("/")[2];
+
+  // if ((day<2 || day >31) || (month<1&&month>12)&&(year.length != 4))
+  // {
+
+  // alert("Invalid Format");return false;
+  // }
+
+  // else
+
+  // {
+
+  // var dt = new Date(year, month-1, day);
+  // var today = new Date();
+
+  // if((dt.getDate() != day) || (dt.getMonth() != month-1) || (dt.getFullYear()!=year) || (dt>today))
+  // {
+
+  // alert("Invalid Date");return false;
+  // }
+
+  // }
+  // var date = new Date();
+  // var tdate = date.getDate();
+  // var month = date.getMonth() + 1;
+  // if (tdate < 10) {
+  //   tdate = '0' + tdate;
+  // }
+  // if (month < 10) {
+  //   month = '0' + month;
+  // }
+  // var year = date.getFullYear();
+  // var minDate = year + "-" + month + "-" + tdate;
+  // document.getElementById("date").setAttribute("min", minDate)
+  // console.log(minDate);
+  document.getElementById('date').setAttribute('min', new Date().toISOString().split('T')[0])
+  // document.getElementById('date').setAttribute('max', new Date().toISOString().split('T')[0])
+
+
+
 }
